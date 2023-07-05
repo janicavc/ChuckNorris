27c07d212061ce14142d7e10ae8877cc2c4a1530
janicavc <crisostomo_janica@yahoo.com>
2023-07-04 20:40:44 -0700

    update

--- controllers/chuck.js
+++ controllers/chuck.js
@@ -1,17 +1,17 @@
    const fetch = require('node-fetch');
-const jokeURL = 'https://api.chucknorris.io/jokes/random?category=';
-const categoriesURL = 'https://api.chucknorris.io/jokes/categories';
+const joke = 'https://api.chucknorris.io/jokes/random?category=';
+const categories = 'https://api.chucknorris.io/jokes/categories';
 
 module.exports = {
   index
 };
 
 function index(req, res) {
-  fetch(categoriesURL)
+  fetch(categories)
     .then(res => res.json())
     .then(categories => {
     if (req.query.category) {
-      fetch(`${jokeURL}${req.query.category}`)
+      fetch(`${joke}${req.query.category}`)
         .then(res => res.json())
         .then(joke => {
           res.render('index', {categories, category: req.query.category, joke: joke.value});
